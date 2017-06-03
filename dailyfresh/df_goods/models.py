c231a5b3dab5277b67430a98789e9bc2618c1d80
re <781816703@qq.com>
2017-06-03 09:47:31 +0800

    商品

--- dailyfresh/df_goods/models.py
+++ dailyfresh/df_goods/models.py
@@ -1,5 +1,11 @@
+#coding=utf-8
 from django.db import models
 
 # Create your models here.
+
+class TypeGoods(models.Model):#商品种类
+    title = models.CharField(max_length=40)
+
+
 class GoodsInfo(models.Model):
     pass
